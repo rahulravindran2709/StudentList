8c8701204bf6929cd06ff3f72f9fcfa0f9ef0752
Dileep Singh <dileep2008apex@gmail.com>
2015-05-02 00:47:11 +0530

    Created Server.js

---
+++ server.js
@@ -0,0 +1,57 @@
+// MEAN Stack RESTful API Tutorial - studentlist
+
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('studentlist', ['studentlist']);
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+app.get('/studentlist', function (req, res) {
+  console.log('I received a GET request');
+
+  db.studentlist.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+app.post('/studentlist', function (req, res) {
+  console.log(req.body);
+  db.studentlist.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete('/studentlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.studentlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/studentlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.studentlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.put('/studentlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.name);
+  db.studentlist.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
+
+app.listen(3000);
+console.log("Server running on port 3000");
